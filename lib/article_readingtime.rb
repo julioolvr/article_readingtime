c628a3e3e764da68be94828ef7855c68b7f1876d
Julio Olivera <julio.olvr@gmail.com>
2016-10-16 13:30:08 -0300

    Counts images

--- lib/article_readingtime.rb
+++ lib/article_readingtime.rb
@@ -3,11 +3,30 @@ require 'article_readingtime/version'
 require 'nokogiri'
 
 WORDS_PER_MINUTE = 275
+IMAGE_MAX = 15
+IMAGE_MIN = 3
+IMAGE_STEP = 1
 
 module ArticleReadingtime
   def self.estimate_html(html)
     doc = Nokogiri::HTML(html)
+    count_text(doc) + count_images(doc)
+  end
+
+  def self.count_text(doc)
     words = doc.inner_text.split(' ')
     (words.length / WORDS_PER_MINUTE.to_f * 60).round
+  end
+
+  def self.count_images(doc)
+    images_count = doc.css('img').length
+
+    images_count.times.reduce(0) do |total, i|
+      total + if IMAGE_MAX - i * IMAGE_STEP > IMAGE_MIN
+                IMAGE_MAX - i
+              else
+                IMAGE_MIN
+              end
+    end
   end
 end
--- spec/article_readingtime_spec.rb
+++ spec/article_readingtime_spec.rb
@@ -7,5 +7,28 @@ describe ArticleReadingtime do
       html = "<div>#{Array.new(word_count) { 'word' }.join(' ')}</div>"
       expect(ArticleReadingtime.estimate_html(html)).to equal 60
     end
+
+    it 'counts the first image as 15 seconds' do
+      html = '<img src="something.jpg"/>'
+      expect(ArticleReadingtime.estimate_html(html)).to equal 15
+    end
+
+    it 'counts the following images one second less each' do
+      html = <<-HTML
+        <img src="something.jpg" />
+        <img src="something2.jpg" />
+        <img src="something3.jpg" />
+      HTML
+
+      expect(ArticleReadingtime.estimate_html(html)).to equal 15 + 14 + 13
+    end
+
+    it 'counts images as the fixed minimum once reached' do
+      image_tag = '<img src="something.jpg" />'
+      html = Array.new(15) { image_tag }.join('')
+      expected_time = 15 + 14 + 13 + 12 + 11 + 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 3 + 3
+
+      expect(ArticleReadingtime.estimate_html(html)).to equal expected_time
+    end
   end
 end
